151e005f26751ed4d1eb58e7f0eb6551d6892314
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-06-03 10:24:58 +0200

    reposition cursor after changes

--- ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.java
@@ -503,8 +503,10 @@ public class ReactEditText extends AppCompatEditText {
       // When we update text, we trigger onChangeText code that will
       // try to update state if the wrapper is available. Temporarily disable
       // to prevent an infinite loop.
+      Integer currentPosition = getSelectionStart();
       getText().clear();
       append(spannableStringBuilder);
+      setSelection(currentPosition);
     }
     mDisableTextDiffing = false;
 
